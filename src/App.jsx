f2a390c934dfca53469e9b619fdb2e996ec2c6fa
zeusCach <zeus.cach@outlook.com>
2026-02-14 01:03:11 -0500

    Crenado UI de summary(style components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import InfoSummary from "./components/InfoSummary"
+import ListExpense from "./components/ListExpense"
+import Summary from "./components/summary"
 
 function App() {
 
 
   return (
     <>
+      <div className="grid grid-cols-2 gap-4 h-screen p-4">
 
-      <h1 className="text-4xl text-center">Hola mundo</h1>
+        <Summary />
+        <ListExpense />
+        <InfoSummary />
+
+      </div>
+
     </>
   )
 }
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,11 +1,49 @@
+import { useState } from "react"
+import { expensesDB } from "../data/data"
 
 
 
 export default function Summary() {
+    const [db] = useState(expensesDB)
+    console.log(db);
+
     return (
         <>
-            <div className="">
-                <h1>Resumen de gastos</h1>
+            <div className="w-full max-w-2xl mx-auto px-10 py-10">
+                <h1 className="text-white text-4xl font-bold text-center pb-5">
+                    Resumen de gastos
+                </h1>
+
+                <div >
+                    {
+                        db.map(item => (
+                            <>
+                                <section className="bg-orange-400 rounded-2xl p-5 shadow-md hover:shadow-lg transition mb-4" key={item.id}>
+                                    <div className="flex justify-between items-start">
+
+                                        <div>
+                                            <p className="text-xl font-medium text-white">
+                                                {item.title}
+                                            </p>
+                                            <p className="text-lg text-gray-900  opacity-80">
+                                                {item.category}
+                                            </p>
+                                        </div>
+
+                                        <p className="text-3xl font-bold text-right text-white mt-1">
+                                            ${item.amount}
+                                        </p>
+                                    </div>
+
+                                </section>
+
+                            </>
+
+
+
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
