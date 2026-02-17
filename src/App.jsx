f1c175ea20a27e44cafe44cb4dc8cf4f9676ce69
zeusCach <zeus.cach@outlook.com>
2026-02-17 02:03:44 -0500

    Corrigiendo estructuras

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+import Footer from "./components/Footer"
+import FormExpense from "./components/FormExpense"
+import Header from "./components/Header"
 import InfoSummary from "./components/InfoSummary"
-import ListExpense from "./components/ListExpense"
-import Summary from "./components/summary"
+import Perfil from "./components/Perfil"
+import Summary from "./components/Summary"
 
 function App() {
-
+  //https://youtube.com/shorts/5AAMKbfcdco?si=SkgoIWPzqae50NtT
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 h-screen p-4">
+      <Header />
 
+      <div className="grid grid-cols-1 min-h-screen  md:grid-cols-2 gap-4  p-5">
         <Summary />
-        <ListExpense />
-        <InfoSummary />
-
+        <FormExpense />
       </div>
-
+      <Footer />
     </>
   )
 }
