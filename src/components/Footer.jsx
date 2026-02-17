5df0115ed01eb1640790a277800d03057d82d0b5
zeusCach <zeus.cach@outlook.com>
2026-02-17 02:07:07 -0500

    Correccion de dominio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function () {
             <footer className="bg-gray-800 p-10 mt-5">
                 <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between">
                     <p className="text-slate-500 font-semibold text-lg">
-                        &copy; {new Date().getFullYear()} Savings - Realizado con 🤟🏼 por
+                        &copy; {new Date().getFullYear()} SummaryExpense - Realizado con 🤟🏼 por
                         <a href="https://www.linkedin.com/in/zeus-cach-715a84328/"> Zeus Cach
                         </a>
                     </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <div className="bg-gray-800 p-4 flex justify-between items-center">
                 <div>
-                    <h1 className="text-purple-500 font-bold text-2xl ">Savings</h1>
+                    <h1 className="text-purple-500 font-bold text-2xl ">SummaryExpense</h1>
                 </div>
                 <Perfil />
             </div>
