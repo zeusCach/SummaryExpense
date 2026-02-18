e2ab44e508a71fec4f70229b510769072fd10953
zeusCach <zeus.cach@outlook.com>
2026-02-17 22:12:43 -0500

    Envolviendo el provider en nuestra app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { UserContextProvider } from './context/userContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+
   </StrictMode>,
 )
