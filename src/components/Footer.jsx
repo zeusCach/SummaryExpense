c3d72a116731f1c739eb32c2fbea131549e8145c
zeusCach <zeus.cach@outlook.com>
2026-02-20 01:24:01 -0500

    Ajustes de UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function () {
             <footer className="bg-neutral-800 p-5 mt-5">
                 <div className="flex flex-col items-center gap-3 md:flex-row md:justify-between">
                     <p className="text-slate-500 font-semibold text-lg text-center">
-                        &copy; {new Date().getFullYear()} SummaryExpense - Realizado con 🤟🏼 por
+                        &copy; {new Date().getFullYear()} SummaryExpense - Por
                         <a href="https://www.linkedin.com/in/zeus-cach-715a84328/"> Zeus Cach
                         </a>
                     </p>
