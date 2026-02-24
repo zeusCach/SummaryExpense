c41ba7134f494a9e07d5f99e1a29d4001d1625ef
zeusCach <zeus.cach@outlook.com>
2026-02-24 01:04:40 -0500

    Saludos, no me acuerdo que hice aqui, creo que nada :)

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,3 @@
 export const expensesDB = [
     { id: 1, title: "Supermercado", amount: 50, category: "Comida" },
-    { id: 2, title: "Netflix", amount: 15, category: "Entretenimiento" },
-    { id: 3, title: "Gasolina", amount: 40, category: "Transporte" },
 ];
