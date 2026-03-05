e720741fef5f75294c5e76e8c1cdfb2c543de9a0
zeusCach <zeus.cach@outlook.com>
2026-03-05 11:33:18 -0500

    Creando rutas para navegacion mobile

--- src/components/NavigatorDown.jsx
+++ src/components/NavigatorDown.jsx
@@ -4,6 +4,7 @@ import {
     LogOutIcon,
     UserCog2Icon
 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 export default function NavigatorDown() {
@@ -12,16 +13,35 @@ export default function NavigatorDown() {
 
             <div className="flex justify-between items-center px-4">
                 <div className="flex flex-col items-center gap-1">
-                    <UserCog2Icon size={30} className="text-white cursor-pointer active:text-emerald-400" />
-                    <p className="text-xs text-gray-400">configuración</p>
+                    <Link to={'/cuenta'}>
+                        <UserCog2Icon 
+                            size={30} 
+                            className="text-white cursor-pointer active:text-emerald-400" 
+                        />
+                    </Link>
+                    <p className="text-xs text-gray-400">
+                        configuración
+                    </p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
-                    <CirclePlus size={30} className="text-white cursor-pointer active:text-emerald-400" />
-                    <p className="text-xs text-gray-400">Añade gasto</p>
+                    <Link to={'/registrar'}>
+                        <CirclePlus 
+                            size={30} 
+                            className="text-white cursor-pointer active:text-emerald-400"
+                        />
+                    </Link>
+                    <p className="text-xs text-gray-400">
+                        Añade gasto
+                    </p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
-                    <ChartLine size={30} className="text-white cursor-pointer active:text-emerald-400" />
-                    <p className="text-xs text-gray-400">Estadísticas</p>
+                    <ChartLine 
+                        size={30} 
+                        className="text-white cursor-pointer active:text-emerald-400" 
+                    />
+                    <p className="text-xs text-gray-400">
+                        Estadísticas
+                    </p>
                 </div>
             </div>
 
