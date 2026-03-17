4b27e7705ab0dd4e1f3b25d8caf488e900ba9319
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-03-17 00:20:50 -0500

    Ajuste de rutas para la vista principal

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import AppRoutes from "./routes/AppRoutes"
 import { useReducer } from "react";
 import { ExpenseReducer, inicialState } from "./reducer/appReducer";
 import NavigatorDown from "./components/NavigatorDown";
+import { useLocation } from "react-router-dom";
 
 
 function App() {
+  //nos brinda la ruta actual
+  const location = useLocation();
+
+  //comprobamos si nuestra ruta actual es el home: devuelve true o false
+  const isHome = location.pathname === "/"
+  console.log(isHome);
+
   return (
     <>
-      <Navigator />
+      {/* condicion de isHome, si la ruta es la principal, solo muestra nuestro home sin footer y sin acceso a la navegacion */}
+      {!isHome && <Navigator />}
       <AppRoutes />
-      <NavigatorDown />
-      <Footer />
+      {!isHome && <NavigatorDown />}
+      {!isHome && <Footer />}
     </>
   )
 }
