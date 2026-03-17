85ef92bf6d1cb846edc208a3600e0afbc20a14c4
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-03-17 00:23:15 -0500

    Creacion de ruta principal para manejo de  la configuracion del home, ajuste de nombre de componente de viewPrincipal a dashboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import ViewPrincipal from "../components/ViewPrincipal";
 import Configuration from "../pages/Configuration";
 import FormExpense from "../pages/FormExpense";
+import Home from "../pages/Home";
+import Dashboard from "../components/Dashboard";
 
 
 export default function AppRoutes(dispatch) {
     return (
         <Routes>
-            <Route path="/" element={<ViewPrincipal />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/cuenta" element={<Configuration />} />
             <Route path="/estadisticas" element={<Configuration />} />
             <Route path="/registrar" element={<FormExpense />} />
