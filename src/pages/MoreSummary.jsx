2d378d4cfb4433cfdde9ac964cc35398601a8f00
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-03-25 23:54:16 -0500

    Creacion de la vista more summary, para renderizar todos nuestros gastos

---
+++ src/pages/MoreSummary.jsx
@@ -0,0 +1,38 @@
+import { useContext } from "react"
+import { ReduceContext } from "../context/reduceContext"
+import CardSummary from "../components/CardSummary"
+import { Link } from "react-router-dom"
+import { ArrowLeftCircle } from "lucide-react"
+
+export default function MoreSummary() {
+    const expenses = useContext(ReduceContext)
+
+    return (
+        <div className="w-full max-w-2xl mx-auto px-5 md:px-0">
+            <div className=" md:flex justify-between items-baseline p-8 ">
+                <div className="pb-4">
+                    <h1 className="text-white text-4xl font-bold">
+                        Resumen de gastos
+                    </h1>
+                    <p className="text-lg text-slate-300">
+                        Estos son tus gastos completos
+                    </p>
+                </div>
+
+                <Link to="/dashboard">
+                    <span className="flex items-center gap-2 text-white hover:text-green-400">
+                        <ArrowLeftCircle />
+                        Ir al dashboard
+                    </span>
+                </Link>
+            </div>
+
+            {expenses.map(expense => (
+                <CardSummary
+                    key={expense.id}
+                    expense={expense}
+                />
+            ))}
+        </div>
+    )
+}
