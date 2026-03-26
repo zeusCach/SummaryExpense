ec38153cc3937634bda8b190313254ce549c2dad
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-03-25 23:55:13 -0500

    Creacion de logica para renderizar solamente 2 cards de gastos si hay mas de 3 creados, para no romper la vista UI y mejorar el UX

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,8 @@
 import { useContext, useMemo, useState } from "react"
-import { expensesDB } from "../data/data"
 import { ReduceContext } from "../context/reduceContext"
 import CardSummary from "./CardSummary";
+import { ArrowRightCircle } from "lucide-react";
+import { Link } from "react-router-dom";
 
 
 export default function Summary() {
@@ -14,13 +15,16 @@ export default function Summary() {
     //funcion que verifica si existen gastos
     const isEmpty = useMemo(() => state.length === 0, [state]);
 
+    //retorna solo 2 gastos si hay mas en nuestro stock de gastos
+    const itemExpense = state.length > 3 ? state.slice(0, 2) : state;
+
     return (
         <>
 
             <div className="w-full max-w-2xl mx-auto">
                 <div className="" >
                     <h1 className="text-white text-4xl font-bold pb-5">
-                        Resumen de gastos
+                        Tus gastos más recientes
                     </h1>
 
 
@@ -28,7 +32,7 @@ export default function Summary() {
                         isEmpty
                             ? <p className="text-lg text-white/80 font-bold py-18">Genial, no tienes gastos. Añade alguno y empieza ahorrar</p>
                             :
-                            state.map(expense => (
+                            itemExpense.map(expense => (
 
                                 <CardSummary
                                     key={expense.id}
@@ -36,7 +40,20 @@ export default function Summary() {
                                 />
 
                             ))
+                    }
 
+                    {
+                        state.length > 3 && (
+                            <Link to='/expenses'>
+                                <span className="
+                                flex items-center gap-2 
+                                text-white hover:text-green-400
+                                px-4 py-4
+                                ">
+                                    Ver todos los gastos <ArrowRightCircle />
+                                </span>
+                            </Link>
+                        )
                     }
                 </div>
             </div>
