ab243d8f0b0a3cb9f159c3df3628b0efedb1c50e
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-03-28 00:49:07 -0500

    Agregando la nueva ruta (Tarjetas)

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -14,15 +14,21 @@ export default function Navigator() {
                         </h1>
                     </Link>
 
-                    <NavLink className="text-slate-500 font-bold" to="/cuenta">Cuenta</NavLink>
+                    <NavLink className="text-slate-500 font-bold" to="/cuenta">
+                        Cuenta
+                    </NavLink>
+
+                    <NavLink className="text-slate-500 font-bold" to="/tarjetas">
+                        Tarjetas
+                    </NavLink>
 
                 </div>
-                <div className="flex justify-end items-center gap-5">
+                {/* <div className="flex justify-end items-center gap-5">
 
-                    <button className="border border-white/40 text-white bg-transparent hover:bg-white/10 px-4 py-2 rounded-lg">
+                    <button className="border border-white/40 text-white bg-transparent hover:bg-white/10 px-2 py-2 rounded-lg">
                         Cerrar sesion
                     </button>
-                </div>
+                </div> */}
 
             </div>
         </>
