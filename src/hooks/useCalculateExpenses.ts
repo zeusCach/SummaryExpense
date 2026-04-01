6a790cb8768f031e1f1770f2117f93d0c9998ce1
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-01 00:29:13 -0500

    Creacion de hook personalizado para crear calculo de gasto y total de dinero

--- src/hooks/useCalculateExpenses.ts
+++ src/hooks/useCalculateExpenses.ts
@@ -3,11 +3,19 @@ import { useAmount } from "./useAmount"
 import { useExpense } from "./useExpense";
 
 export const useCalculateExpense = () => {
-    const { state } = useExpense();
-    const { } = useAmount();
-    console.log(state)
+    const { state: stateExpenses } = useExpense();
+    const { state: stateAmount } = useAmount();
 
-    //operacion que hace el calculo de gasto
+    //operacion que hace el calculo de gasto total
+    const totalGastado = stateExpenses.expenses.reduce((acc, expenses) => {
+        return acc + expenses.amount
+    }, 0)
 
+    //calcula el total disponible de nuestro dinero
+    const totalDisponible = stateAmount.amount - totalGastado;
 
+    return {
+        totalGastado,
+        totalDisponible
+    }
 }
