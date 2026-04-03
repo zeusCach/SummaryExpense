075f79c4ff6457e8d88800f66a7780a59fa7efb7
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-03 01:18:56 -0500

    Creacion de feature para actualizar tu dinero

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -2,6 +2,7 @@
 import Account from "../components/ConfigurationUI/Account";
 import Password from "../components/ConfigurationUI/Password";
 import Danger from "../components/ConfigurationUI/Danger";
+import MoneySettings from "../components/ConfigurationUI/MoneySettings";
 
 
 export default function Configuration() {
@@ -11,6 +12,7 @@ export default function Configuration() {
         <>
             <div className="container mx-auto max-w-4xl px-5 py-10">
                 <Account />
+                <MoneySettings />
                 <Password />
                 <Danger />
             </div>
