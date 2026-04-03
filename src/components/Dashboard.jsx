297df5b6778fed378c8718e78e1cec947798c97e
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-03 01:14:16 -0500

    Correccion de navegacion

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import InfoSummary from "./InfoSummary";
 import Summary from "./Summary";
-import { useAmount } from "../hooks/useAmount";
-import { amountContext } from "../context/amountContext";
+import { useUser } from "../hooks/useUser";
+import { useCalculateExpense } from "../hooks/useCalculateExpenses";
 
 
 export default function Dashboard() {
-    const { state } = useAmount();
-    console.log(state.amount)
+    const { state } = useUser();
+    const navigate = useNavigate();
+    const { excedePresupuesto } = useCalculateExpense();
     return (
         <>
             <div className="flex items-center justify-between m-10">
                 <p className="text-slate-500 font-bold">
-
+                    @{state.user}
                 </p>
 
-                <Link to='/registrar' className="bg-orange-500 hover:bg-orange-600 text-white font-bold px-4 py-2 rounded-lg">
+                <button
+                    disabled={excedePresupuesto}
+                    onClick={() => navigate('/registrar')}
+                    className={`bg-orange-500 hover:bg-orange-600 text-white 
+                        font-bold px-4 py-2 rounded-lg cursor-pointer
+                        disabled:opacity-50 disabled:cursor-not-allowed`}
+                >
                     Añadir gasto
-                </Link>
+                </button>
+
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-8 pb-8 flex-1 overflow-hidden">
