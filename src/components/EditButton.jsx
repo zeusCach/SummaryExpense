547f0f1114beb8ab5df9202d430ed027cb27a747
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-03 01:14:44 -0500

    Optimizando componente reutilizable para futuras acciones reutilizables

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,25 +1,18 @@
-import { useState } from "react";
-
-
-export default function EditButton(setIsEdit) {
-
+export default function EditButton({ onSave, onCancel }) {
     return (
-        <>
-            <div className="flex gap-2">
-                <button
-
-                    className="px-2 py-2 bg-green-500  text-white font-medium rounded-xl cursor-pointer "
-                >
-                    Guardar
-                </button>
-
-                <button
-
-                    className="px-2 py-2 bg-red-400 text-white font-medium rounded-xl cursor-pointer"
-                >
-                    Cancelar
-                </button>
-            </div>
-        </>
+        <div className="flex gap-2 ">
+            <button
+                onClick={onSave}
+                className="px-2 py-2 bg-green-500 text-white font-medium rounded-xl cursor-pointer"
+            >
+                Guardar
+            </button>
+            <button
+                onClick={onCancel}
+                className="px-2 py-2 bg-red-400 text-white font-medium rounded-xl cursor-pointer"
+            >
+                Cancelar
+            </button>
+        </div>
     )
 }
