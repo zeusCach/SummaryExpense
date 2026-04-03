04fbaa5f0b6ae8cdead96fba69151f266a649bd9
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-03 01:14:00 -0500

    Añadiendo provider del context user y correccion de algunos

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { UserContextProvider } from './context/userContext.jsx'
+
 import { BrowserRouter } from 'react-router-dom'
 
 import CardContextProvider from './context/cardContext.jsx'
 import AmountContextProvider from './context/amountContext.js'
 import ExpensesContextProvider from './context/ExpensesContext.jsx'
+import UserContextProvider from './context/userContext.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <UserContextProvider>
-        <ExpensesContextProvider>
-          <CardContextProvider>
-            <AmountContextProvider>
+
+      <ExpensesContextProvider>
+        <CardContextProvider>
+          <AmountContextProvider>
+            <UserContextProvider>
               <App />
-            </AmountContextProvider>
-          </CardContextProvider>
-        </ExpensesContextProvider>
-      </UserContextProvider>
+            </UserContextProvider>
+          </AmountContextProvider>
+        </CardContextProvider>
+      </ExpensesContextProvider>
+
     </BrowserRouter>
   </StrictMode>,
 )
