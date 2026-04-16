c2f68b5cddd460dcd73d833751bf26f0fdbfd5fa
Zeus Cach <83797099+zeusCach@users.noreply.github.com>
2026-04-16 01:50:59 -0500

    Migrando reducer a typescript

--- src/reducer/cardReducer.ts
+++ src/reducer/cardReducer.ts
@@ -1,25 +1,20 @@
 import { v4 as uuidv4 } from "uuid"
+import { Card } from "../types"
 
-//Creacion de las acciones de nuestras tarjetas
+//Tipamos nuestro state principal
+export type CardState = {
+    card: Card[]
+}
+//centralizamos los tipos de accion que hay
 export const CARD_TYPE = {
     ADD: 'add-card',
     DELETE: 'delete-card',
     UPDATE: 'update-card'
-}
+} as const
 
 //CardAction, controlador de las acciones que ejecutara nuestro reducee
-export const CardAction = {
-    addCard: (card) => ({
-        type: CARD_TYPE.ADD,
-        payload: card
-    }),
-    updateCard: () => ({
-        //Pendienteeeee
-    }),
-    deleteCard: () => ({
-        //Pendienteeeee
-    })
-}
+export type CardAction =
+    { type: typeof CARD_TYPE.ADD, payload: Card }
 
 //localstorage
 const localStorageCard = () => {
@@ -28,12 +23,14 @@ const localStorageCard = () => {
 }
 
 //state incial de nuestro reducer (falta poner localStorage)
-export const inicialStateCard = {
+export const inicialStateCard: CardState = {
     card: localStorageCard()
 }
 
 //reducer principar, la fuente de verdad jejej
-export const CardReducer = (state, action) => {
+export const CardReducer = (
+    state: CardState = inicialStateCard, action: CardAction
+) => {
     switch (action.type) {
         case (CARD_TYPE.ADD):
             return {
